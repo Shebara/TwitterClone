b80a3d1d610303707824fe77314680522bb1ae1b
Nebojša Jarić <jaricn@gmail.com>
2023-04-04 22:23:06 +0200

    Initialize single post page

--- src/modules/routes.js
+++ src/modules/routes.js
@@ -1,4 +1,5 @@
 import PostList from '../pages/PostList.vue';
+import Post from '../pages/SinglePost.vue';
 import Error404 from '../pages/404.vue';
 
 const routes = [
@@ -6,6 +7,11 @@ const routes = [
         name: 'Homepage',
         path: '/',
         component: PostList
+    },
+    {
+        name: 'Single Post',
+        path: '/post/:id',
+        component: Post
     },
     {
         name: '404',
---
+++ src/pages/SinglePost.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="post-list">
+    <h1>Single Post</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SinglePost',
+  data() {
+    return {
+      post: {}
+    }
+  },
+  async created() {
+    try {
+      const res = await this.axios.get(`http://localhost:3000/posts`);
+
+      this.posts = res.data;
+    } catch(e) {
+      console.error(e);
+    }
+  }
+}
+</script>
