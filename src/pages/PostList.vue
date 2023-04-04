acebaa6c4f37ed43949ffd9e9c04f6f1fca1651c
Nebojša Jarić <jaricn@gmail.com>
2023-04-04 22:18:39 +0200

    Implement Post List

--- src/pages/PostList.vue
+++ src/pages/PostList.vue
@@ -1,12 +1,35 @@
 <template>
-  <div class="hello">
+  <div class="post-list">
     <h1>Post List</h1>
-    <!--TODO-->
+    <div class="posts flex flex-wrap justify-center">
+      <div class="p-4 border" v-for="post of posts" :key="post.id">
+        <router-link :to="`/post/${post.id}`" class="text-left hover:text-black-70 underline text-black">{{ post.content }}</router-link>
+        <div class="mt-2 text-right text-cs">Posted on
+          <router-link :to="`/post/${post.id}`" class="text-left hover:text-black-70 underline text-black">{{ post.dateCreated }}</router-link>
+          by
+          <router-link :to="`/post/${post.id}`" class="text-left hover:text-black-70 underline text-black">{{ post.authorId }}</router-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PostList'
+  name: 'PostList',
+  data() {
+    return {
+      posts: []
+    }
+  },
+  async created() {
+    try {
+      const res = await this.axios.get(`http://localhost:3000/posts`);
+
+      this.posts = res.data;
+    } catch(e) {
+      console.error(e);
+    }
+  }
 }
 </script>
