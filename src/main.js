61ad19bf029939577f96df4b5354a7902a8055cc
Nebojša Jarić <jaricn@gmail.com>
2023-04-04 23:05:36 +0200

    Extract router.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,14 @@
 import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import VueRouter from 'vue-router'
 
 import App from './App.vue'
-import routes from './modules/routes.js'
+import router from './modules/router.js'
 import store from './modules/store.js'
 
 import './assets/css/tailwind.css'
 
-Vue.use(VueRouter)
 Vue.use(VueAxios, axios)
-
-const router = new VueRouter({ mode: 'history', routes: routes })
 
 Vue.config.productionTip = false
 
