6535c44ab22a3563048f19df4b060744c34dca7d
Nebojša Jarić <jaricn@gmail.com>
2023-04-04 22:59:51 +0200

    Init authentication pages

---
+++ src/pages/LogIn.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="log-in">
+    <h1>Log In</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LogIn',
+  data() {
+    return {
+      user: {}
+    }
+  },
+}
+</script>
