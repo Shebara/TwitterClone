c812a10947c9ce0cb16f8447db99869f92ad5427
Nebojša Jarić <jaricn@gmail.com>
2023-04-04 23:36:51 +0200

    Force log in

--- src/modules/router.js
+++ src/modules/router.js
@@ -7,4 +7,15 @@ Vue.use(VueRouter)
 
 const router = new VueRouter({ mode: 'history', routes: routes })
 
+router.beforeEach((to, from, next) => {
+    const authRequired = ! ['/login', '/register'].includes(to.path)
+    const loggedIn = localStorage.getItem('user')
+
+    if (authRequired && ! loggedIn) {
+      return next('/login')
+    }
+
+    next()
+})
+
 export default router;
