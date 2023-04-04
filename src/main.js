dc988589030ca07403910f13b4b90ad86f225077
Nebojša Jarić <jaricn@gmail.com>
2023-04-04 21:39:29 +0200

    Implement Vuex store

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueRouter from 'vue-router'
 
 import App from './App.vue'
 import routes from './modules/routes.js'
+import store from './modules/store.js'
 
 import './assets/css/tailwind.css'
 
-Vue.use(VueRouter);
-Vue.use(Vuex);
-Vue.use(VueAxios, axios);
+Vue.use(VueRouter)
+Vue.use(VueAxios, axios)
 
-const router = new VueRouter({ mode: 'history', routes: routes });
+const router = new VueRouter({ mode: 'history', routes: routes })
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount('#app')
