7e1866a867bfdcd6916cd4533af0e980e7e08919
Nebojša Jarić <jaricn@gmail.com>
2023-04-04 23:37:23 +0200

    Prepare store for authentication logic

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 import App from './App.vue'
-import router from './modules/router.js'
 import store from './modules/store.js'
+import router from './modules/router.js'
 
 import './assets/css/tailwind.css'
 
--- src/modules/store.js
+++ src/modules/store.js
@@ -1,12 +1,56 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import router from './router.js'
 
 Vue.use(Vuex)
 
 const state = {
-    posts: []
+    isUser: false,
+    token: false,
+    user: false,
+}
+
+const actions = {
+    login({commit}, {user}) {
+        commit('loginRequest')
+
+        if (user.token) {
+            commit('loginSuccess', user)
+            console.log('Logged in!')
+            router.push('/')
+        } else {
+            commit('loginFailure')
+        }
+    },
+    logout({commit}) {
+        commit('resetState')
+        router.push('/')
+    }
+}
+
+const mutations = {
+    resetState(state) {
+        state.isLogin = false;
+        state.token = false;
+        state.user = false;
+    },
+    loginRequest(state) {
+        state.isLogin = true;
+        state.token = false;
+        state.user = false;
+    },
+    loginSuccess(state, result) {
+        state.isLogin = true;
+        state.token = result.token;
+        state.user = result.user;
+    },
+    loginFailure(state) {
+        state.isLogin = false;
+    },
 }
 
 export default new Vuex.Store({
-    state
+    state,
+    actions,
+    mutations
 })
