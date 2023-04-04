d840aca5c63dfc3cde9a0d52e4c5084bda46214f
Nebojša Jarić <jaricn@gmail.com>
2023-04-04 22:53:09 +0200

    Initiate single user

---
+++ src/pages/SingleUser.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="single-user">
+    <h1>Single User</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SinglePost',
+  data() {
+    return {
+      user: {}
+    }
+  },
+  async created() {
+    try {
+      const res = await this.axios.get(`http://localhost:3000/users/${this.$route.params.id}`);
+
+      this.user = res.data;
+    } catch(e) {
+      console.error(e);
+    }
+  }
+}
+</script>
