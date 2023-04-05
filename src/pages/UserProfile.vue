27c95aede5ffcbb0828923d3f1fceb42215b3295
Nebojša Jarić <jaricn@gmail.com>
2023-04-06 00:58:31 +0200

    List posts on user profile

--- src/pages/UserProfile.vue
+++ src/pages/UserProfile.vue
@@ -4,12 +4,29 @@
     <img class="mx-auto max-w-sm" v-show="avatar" :src="avatar" :alt="name" />
     <h5>@{{ name }}</h5>
     <p>Registered on: {{ registrationDate | moment("calendar") }}</p>
+    <h2>Posts by @{{ name }}</h2>
+    <div class="posts flex flex-wrap justify-center mt-8">
+      <ListPost
+        v-for="post of posts"
+        :key="post.id"
+        :id="post.id"
+        :content="post.content"
+        :authorId="post.authorId"
+        :datePublished="post.datePublished"
+        :noUserData=true
+        />
+    </div>
   </div>
 </template>
 
 <script>
+import ListPost from '../components/ListPost.vue'
+
 export default {
   name: 'UserProfile',
+  components: {
+    ListPost
+  },
   data() {
     return {
       id: false,
@@ -17,6 +34,7 @@ export default {
       name: false,
       avatar: null,
       registrationDate: false,
+      posts: []
     }
   },
   async created() {
@@ -29,6 +47,9 @@ export default {
       this.displayName = user.displayName;
       this.avatar = user.avatar;
       this.registrationDate = user.registrationDate;
+
+      const posts = await this.axios.get(`http://localhost:3000/posts?authorId=${this.id}`);
+      this.posts = posts.data;
     } catch(e) {
       console.error(e);
     }
