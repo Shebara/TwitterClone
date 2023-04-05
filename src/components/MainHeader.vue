8ba0456f768b6b64cf63aaaa5f000c6a330f9fe8
Nebojša Jarić <jaricn@gmail.com>
2023-04-05 19:10:28 +0200

    Auth-dependent header links

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,11 +1,18 @@
 <template>
     <header class="p-5 bg-green-300 border-b border-indigo-900">
       <div class="container mx-auto text-indigo-900">
-        <ul class="main-menu list-none flex gap-5 justify-center">
-          <li v-for="link of links" :key="link.url">
-            <router-link :to="link.url" class="hover:text-indigo-500">{{ link.text }}</router-link>
-          </li>
-        </ul>
+        <div class="menus flex justify-between">
+          <ul class="main-menu list-none flex gap-5 justify-left">
+            <li v-for="link of links" :key="link.url">
+              <router-link :to="link.url" class="hover:text-indigo-500">{{ link.text }}</router-link>
+            </li>
+          </ul>
+          <ul class="auth-menu list-none flex gap-5 justify-right">
+            <li v-for="link of authentication" :key="link.url">
+              <router-link :to="link.url" class="hover:text-indigo-500">{{ link.text }}</router-link>
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
 </template>
@@ -15,7 +22,13 @@ export default {
   name: 'MainHeader',
   data() {
     return {
-      links: [
+      links: [],
+      authentication: []
+    }
+  },
+  created() {
+    if ( this.$store.getters.getUserId !== false ) {
+      this.links = [
         {
           url: '/',
           text: 'Homepage',
@@ -25,8 +38,26 @@ export default {
           text: 'All Posts',
         },
       ]
+      this.authentication = [
+        {
+          url: '/logout',
+          text: 'Log Out'
+        }
+      ]
+    } else {
+      this.links = []
+      this.authentication = [
+        {
+          url: '/login',
+          text: 'Log In'
+        },
+        {
+          url: '/register',
+          text: 'Register'
+        }
+      ]
     }
-  },
+  }
 }
 </script>
 
