6824bdaebbabf372c45ffc4f3d319d9e788e519b
Nebojša Jarić <jaricn@gmail.com>
2023-04-06 09:33:43 +0200

    Initiate SearchUsers

---
+++ src/pages/SearchUsers.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search-users">
+    <h1>Search for Users</h1>
+    <form @submit.prevent="userSearch">
+      <input type="text" v-model="search" placeholder="Search terms" />
+      <button type="submit">Search</button>
+    </form>
+    <div class="users flex flex-wrap justify-center">
+      //
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchUsers',
+  data() {
+    return {
+      search: ''
+    }
+  },
+  methods: {
+    async userSearch() {
+      //
+    }
+  }
+}
+</script>
